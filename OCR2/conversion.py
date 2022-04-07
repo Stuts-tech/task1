1d502716694a724dcb199da2443f36115c7e30c8
Stuts-tech <stuti.dumre@gmail.com>
2022-04-07 13:01:43 +0545

    contour

--- OCR2/conversion.py
+++ OCR2/conversion.py
@@ -1,3 +1,4 @@
+from cgi import test
 import numpy as np
 import imutils
 import cv2
@@ -23,42 +24,15 @@ keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPL
 contours = imutils.grab_contours(keypoints)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
+
+
 location = None
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 10, True)
     if len(approx) == 4:
         location = approx
         break
-
-"""
-mask = np.zeros(edged.shape, np.uint8)
-new_image = cv2.drawContours(mask, [location], (36,25,12), 2)
-new_image = cv2.bitwise_and(img, img, mask=mask)
-(x,y) = np.where(mask==255)
-(x1, y1) = (np.min(x), np.min(y))
-(x2, y2) = (np.max(x), np.max(y))
-cropped_image = gray[x1:x2+1, y1:y2+1]"""
-
-width=800
-height=800
-
-p1=np.ndarray(location)
-p2=np.float32([[0,0],[width,0],[0,height],[width,height]])
-matrix=cv2.getPerspectiveTransform(p1,p2)
-imgoutput=cv2.warpPerspective(img,matrix,(width,height))
-
-for x in range(0,4):
-    cv2.circle(img,(p1[x][0],p2[x][1]),5,(0,0,255),cv2.FILLED)
+   
 
 
-cv2.imshow('original_img',img)
-cv2.imshow('output',imgoutput)  
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-
-    
-print(type(location))
+#rect = cv2.minAreaRect(location)
